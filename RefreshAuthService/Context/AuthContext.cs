60807e954fb510793c1f08db6bda97e4498db2bf
Apoorv Kansal <apoorvkansalak@gmail.com>
2021-01-11 22:36:46 +0530

    Added Helper functions
    Added AuthContext and Repo for interacting with the database and Add and Remove Refresh Tokens from DB

---
+++ RefreshAuthService/Context/AuthContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using RefreshAuthService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RefreshAuthService.Context
+{
+    // This class will be used to interface with Database
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext() : base("RefreshAuthService")
+        {
+
+        }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
---
+++ RefreshAuthService/Context/AuthRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RefreshAuthService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace RefreshAuthService.Context
+{
+    public class AuthRepository : IDisposable
+    {
+        private AuthContext _ctx;
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository()
+        {
+            _ctx = new AuthContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
+        }
+
+        public Client FindClient(string clientId)
+        {
+            return _ctx.Clients.Find(clientId);
+        } 
+
+        // @TODO : Add a Add CLient Class
+        public async Task<bool> AddRefreshToken(RefreshToken token)
+        {
+            // Check if there is existing token for this user and client
+            // If token is there -> Remove the token and if not Add the token
+            var prevRefreshToken = _ctx.RefreshTokens.Where(r => r.Client == token.Client && r.User == token.User).SingleOrDefault();
+            if(prevRefreshToken != null)
+            {
+                // Delete the pre-existing token
+                var result = await RemoveRefreshToken(prevRefreshToken);
+            }
+
+            // After token is deleted or token is not there, 
+            // Add the new token
+            _ctx.RefreshTokens.Add(token);
+            return await _ctx.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> RemoveRefreshToken(RefreshToken refreshToken)
+        {
+            _ctx.RefreshTokens.Remove(refreshToken);
+            return await _ctx.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> RemoveRefreshToken(string refreshTokenId)
+        {
+            var token = await _ctx.RefreshTokens.FindAsync(refreshTokenId);
+            if(token!= null)
+            {
+                _ctx.RefreshTokens.Remove(token);
+                return await _ctx.SaveChangesAsync() > 0;
+            }
+            return false;
+        }
+
+        public async Task<RefreshToken> FindRefreshToken(string refreshTokenId)
+        {
+            return await _ctx.RefreshTokens.FindAsync(refreshTokenId);
+        }
+
+        public List<RefreshToken> GetAllRefreshTokens()
+        {
+            return _ctx.RefreshTokens.ToList();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+            _userManager.Dispose();
+        }
+    }
+}
