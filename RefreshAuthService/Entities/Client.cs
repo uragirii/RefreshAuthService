9e105994067361ff704e1cdaad83aaf25f0ed94d
Apoorv Kansal <apoorvkansalak@gmail.com>
2021-01-11 22:05:44 +0530

    Added Client and RefreshToken Entitites Implementations
    Client represents a basic OAuth Client and RefreshToken will be used to communicate with database

---
+++ RefreshAuthService/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RefreshAuthService.Entities
+{
+    public class Client
+    {
+        [Key]
+        [Required]
+        public string ClientId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ RefreshAuthService/Entities/RefreshToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RefreshAuthService.Entities
+{
+    public class RefreshToken
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Client { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string User { get; set; }
+        [Required]
+        public string ProtectedTicket { get; set; }
+        public DateTime IssuedUtc { get; set; }
+        public DateTime ExpiresUtc { get; set; }
+
+    }
+}
