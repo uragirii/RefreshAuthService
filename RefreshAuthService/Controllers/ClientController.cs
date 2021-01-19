77dced771e0974180e26db1765f42efb4efa45d5
Apoorv Kansal <apoorvkansalak@gmail.com>
2021-01-19 15:33:52 +0530

    [FEAT] Added CORS

--- RefreshAuthService/Controllers/ClientController.cs
+++ RefreshAuthService/Controllers/ClientController.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace RefreshAuthService.Controllers
 {
     public class ClientController : ApiController
     {
         [AllowAnonymous]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
         public Client Post([FromBody]Client client)
         { 
             using (AuthRepository _repo = new AuthRepository())
